ccf07e7f527b7df3e5b11c17d48c0e66804d7f39
Howard Su <howard0su@gmail.com>
2024-11-17 16:56:07 +0800

    remove unused projects

--- projects/sdr_receiver_master/app/start.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-
-apps_dir=/media/mmcblk0p1/apps
-
-source $apps_dir/stop.sh
-
-cat $apps_dir/sdr_receiver_master/sdr_receiver_master.bit > /dev/xdevcfg
-
-$apps_dir/sdr_receiver_master/sdr-receiver-master &
