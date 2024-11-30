e288c16ab4fbaab31a1d80ecb7a6112f846daac8
Howard Su <howard0su@gmail.com>
2024-11-30 23:40:56 +0800

    Fix script

--- projects/sdr_receiver_hpsdr/app/start.sh
+++ projects/sdr_receiver_hpsdr/app/start.sh
@@ -4,20 +4,6 @@ apps_dir=/media/mmcblk0p1/apps
 
 source $apps_dir/stop.sh
 
-#Check clock freq whether equal to 122.88Mhz
-
-freq=$(fw_printenv clk_freq | sed 's/[^0-9]//g')
-if [ "$freq" == "122880000" ]
-then
-  echo "Freq is 122.88Mhz, HPSDR init"
-else
-  echo "System will set to 122880000 Hz after auto reboot."
-  echo "System will reboot."
-  fw_setenv clk_freq 122880000
-  reboot
-fi
-
 cat $apps_dir/sdr_receiver_hpsdr/sdr_receiver_hpsdr.bit > /dev/xdevcfg
-
 
 $apps_dir/sdr_receiver_hpsdr/sdr-receiver-hpsdr
