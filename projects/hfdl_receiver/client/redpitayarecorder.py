a37930e2bdc55fad99bc0e3bd76c5a6817ea103e
Matthias Wirth <matthias.wirth@gmail.com>
2025-03-24 12:00:55 +0000

    redpitayarecorder: fix non-detection of network errors
    use socket.MSG_WAITALL with socket.SO_RCVTIMEO
    
    this is important so there is a reconnect / disconnect if the network
    drops completely as in a network cable being unplugged or the receiver
    losing power suddenly

--- projects/hfdl_receiver/client/redpitayarecorder.py
+++ projects/hfdl_receiver/client/redpitayarecorder.py
@@ -12,8 +12,17 @@ import traceback
 
 SAMPLE_SIZE=4  # Sample size of received data (CS16)
 
+previousLogMsg = None
+previousLogTime = 0
+
+# omit duplicate log messages within 10 seconds
+logDedupTime = 10
+
 def log(msg):
-    milliseconds = math.floor(math.modf(time.time())[0] * 1000)
+    global previousLogMsg
+    global previousLogTime
+    now = time.time()
+    milliseconds = math.floor(math.modf(now)[0] * 1000)
     timestamp = (
             time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
             + ".{0:03.0f}Z ".format(milliseconds)
@@ -28,43 +37,64 @@ def log(msg):
     if not msg.endswith('\n'):
         msg += '\n'
 
+    if now < previousLogTime + logDedupTime and msg == previousLogMsg:
+        return
+
+    previousLogMsg = msg
+    previousLogTime = now
     sys.stderr.write(timestamp + msg)
 
 def read_and_separate_data(device_ip, device_port, device_freq, device_corr):
     # Open a socket to the device
     lastConnect = 0
     while True:
+        # general network timeout and minimum time between trying to connect
+        timeout = 1
         elapsed = time.time() - lastConnect
-        if elapsed < 5:
-            time.sleep(5 - elapsed)
+        if elapsed < timeout:
+            time.sleep(timeout - elapsed)
         lastConnect = time.time()
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             try:
-                s.settimeout(5)
+                s.settimeout(timeout)
                 s.connect((device_ip, device_port))
+
+                # set a timeout for receive (and send for good measure)
+                # timeval is a struct composed of seconds and microseconds both as 64bit values
+                timeval = pack("<qq", 2, 0 * 1000)
+                s.setsockopt(socket.SOL_SOCKET, socket.SO_RCVTIMEO, timeval)
+                s.setsockopt(socket.SOL_SOCKET, socket.SO_SNDTIMEO, timeval)
+                # setting this timeout is important for major network issues like a sudden
+                # unplugging of the newtork cable
+                # if it is not set, send and receive can apparently block indefinitely (in python?)
+
             except OSError as e:
-                log(traceback.format_exception_only(e))
+                log([f"connect to {device_ip}:{device_port}: "] + traceback.format_exception_only(e))
                 continue
             except Exception as e:
-                log(traceback.format_exception(e))
+                log([f"connect to {device_ip}:{device_port}: "]+ traceback.format_exception(e))
                 break
 
             try:
-                s.settimeout(None) # necessary for msg_waitall
+                s.settimeout(None) # necessary for socket.MSG_WAITALL
                 log(f"RX thread connected to {device_ip}:{device_port}")
 
                 # Send the specific byte string to the receiver immediately after connecting
                 connection_message = pack("<1I", int((1.0 + 1e-6 * device_corr) * device_freq))
                 s.sendall(connection_message)
 
+                bufSize = BUFFER_SIZE * SAMPLE_SIZE
+                buf = bytearray(bufSize)
+
                 while True:
-                    # Read a chunk of interleaved data (buffer is always full due to MSG_WAITALL)
-                    data = s.recv(BUFFER_SIZE * SAMPLE_SIZE, socket.MSG_WAITALL)
-                    if len(data) < BUFFER_SIZE * SAMPLE_SIZE:
-                        s.close()
-                        break  # Exit the loop if less than a full chunk is received
-                    sys.stdout.buffer.write(bytes(data))
+                    result = s.recv_into(buf, bufSize, socket.MSG_WAITALL)
+                    if result != bufSize:
+                        break # Exit the loop if less than a full chunk is received
+
+                    sys.stdout.buffer.write(buf)
+
             except OSError as e:
+                #log(traceback.format_exception_only(e))
                 pass
             except Exception as e:
                 log(traceback.format_exception(e))
@@ -78,7 +108,7 @@ def parse_arguments():
     parser.add_argument('--port', '-p', type=int, default=9000, help="Port to receive data from the device (default: 9000)")
     parser.add_argument('--freq', '-f', type=float, help="Center frequency in kHz (required)")
     parser.add_argument('--corr', '-c', type=float, default=0.0, help="PPM correction to apply to the center frequency")
-    parser.add_argument('--buffer_size', '-b', type=int, default=1024, help="Size of each buffer (default: 1024, unit: samples)")
+    parser.add_argument('--buffer_size', '-b', type=int, default=4096, help="Size of each buffer (default: 4096, unit: samples)")
     return parser.parse_known_args()[0]
 
 if __name__ == '__main__':
