5b00c1a378c6fede037e39f876129f62fa4928d0
Matthias Wirth <matthias.wirth@gmail.com>
2025-04-17 07:04:42 +0000

    add command line in start.sh to run program inside gdb

--- projects/hfdl_receiver/app/start.sh
+++ projects/hfdl_receiver/app/start.sh
@@ -6,4 +6,6 @@ source $apps_dir/stop.sh
 
 cat $apps_dir/hfdl_receiver/hfdl_receiver.bit > /dev/xdevcfg
 
+# gdb -batch -ex 'set confirm off' -ex 'handle SIGTERM nostop print pass' -ex 'handle SIGINT nostop print pass' -ex run -ex 'bt full' --args $apps_dir/hfdl_receiver/hfdl-receiver >> /run/receiver.log 2>&1 &
+
 $apps_dir/hfdl_receiver/hfdl-receiver >> /run/receiver.log 2>&1 &
